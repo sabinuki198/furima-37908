5add26d6ddb3fc78be418b905e8bcfb5538de07d
sabinuki198 <dokkoi00@gmail.com>
2022-05-12 16:53:02 +0900

    ユーザー管理機能実装
    ユーザーサインイン、ログインパスを各ビューファイルへ記述。
    サインイン失敗時、エラーメッセージ出るように記述
    Userモデルバリデーションを整列

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
+
+
 
   private
 
@@ -8,4 +11,9 @@ class ApplicationController < ActionController::Base
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kana, :last_kana, :birthday])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
+         validates :nickname,   presence: true
          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+         validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
          validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :birthday, presence: true
+         validates :last_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :birthday,   presence: true
 
          has_many :items
          has_many :purchases
