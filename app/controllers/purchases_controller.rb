4f3979629468ebb90b33ce8a7c714d5dbc3047b2
sabinuki198 <dokkoi00@gmail.com>
2022-05-20 15:21:00 +0900

    formオブジェクトを用いた決済画面の実装
    ・formオブジェクトを用いた決済画面の実装
    ・purchase/indexビューの表示を編集
    ・購入された商品に関するアクションを追記（sold outの表示、編集、購入を出来ないように変更）

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,32 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:index]
+  before_action :item_find, only: [:index, :create]
   def index
     @purchase_shipping_address  = PurchaseShippingAddress.new
+    if @item.user_id == current_user.id
+      redirect_to root_path 
+    elsif @item.purchase.present?
+      redirect_to root_path 
+    end
   end
 
+  def create
+    @purchase_shipping_address  = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+ 
 
-  
+  private
+  def purchase_params
+    params.require(:purchase_shipping_address).permit(:postal_code, :area_id, :municipality, :address, :building, :tel, :item_id, ).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
 end
